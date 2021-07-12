6675a5911786a23d64ca971cadc739eeb98f97c6
farzanehHaddadi <farzhadadi@gmail.com>
2021-07-12 15:54:02 +0430

    router completed

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Home from "./HomePage";
+import Error from "./ErrorPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import PrivateRoute from "./PrivateRoute";
+export {
+  About,
+  Cart,
+  Home,
+  Checkout,
+  Error,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+};
