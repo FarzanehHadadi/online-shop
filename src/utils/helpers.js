79d0eb0b7e48aea7d190e114d2360242a8d07960
farzanehHaddadi <farzhadadi@gmail.com>
2021-07-14 20:13:31 +0430

    Featured productcompleted

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "EUR",
+    minimumFractionDigits: 3,
+  }).format(price / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
