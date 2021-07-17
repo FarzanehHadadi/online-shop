7574e4f23eb36212c06c6856479c5f659d61cf0b
farzanehHaddadi <farzhadadi@gmail.com>
2021-07-17 13:18:24 +0430

    sort products completed

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,7 +17,37 @@ const filter_reducer = (state, action) => {
         all_products: [...action.payload],
         filtered_products: [...action.payload],
       };
-
+    case SET_GRIDVIEW:
+      return { ...state, grid_view: true };
+    case SET_LISTVIEW:
+      return { ...state, grid_view: false };
+    case UPDATE_SORT:
+      return { ...state, sort: action.payload };
+    case SORT_PRODUCTS: {
+      const { sort, filtered_products } = state;
+      let temp_products = [...filtered_products];
+      if (sort === "price-lowest") {
+        temp_products = temp_products.sort(
+          (cur, next) => cur.price - next.price
+        );
+      }
+      if (sort === "price-highest") {
+        temp_products = temp_products.sort(
+          (cur, next) => next.price - cur.price
+        );
+      }
+      if (sort === "name-a") {
+        temp_products = temp_products.sort((cur, next) =>
+          cur.name.localeCompare(next.name)
+        );
+      }
+      if (sort === "name-z") {
+        temp_products = temp_products.sort((cur, next) =>
+          next.name.localeCompare(cur.name)
+        );
+      }
+      return { ...state, filtered_products: temp_products };
+    }
     default:
       throw new Error(`No Matching "${action.type}" - action type`);
   }
