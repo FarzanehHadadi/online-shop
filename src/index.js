0ec4f02fd03d8e35277a0333764aacd45e987692
farzanehHaddadi <farzhadadi@gmail.com>
2021-07-18 23:45:55 +0430

    add auth0 to project

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-z1ah5jcw.us.auth0.com"
+    clientId="C5prR3e4PK0AGChNv27GHyPTXlEqCfzS"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
